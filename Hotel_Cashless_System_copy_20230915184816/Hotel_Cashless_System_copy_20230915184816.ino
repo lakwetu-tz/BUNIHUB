87a5b11422c1f11f3393f2d83dc17117d385c3e1
Lakwetu Systems <enocrunk@gmail.com>
2023-09-23 21:29:22 +0300

    done

--- Hotel_Cashless_System_copy_20230915184816/Hotel_Cashless_System_copy_20230915184816.ino
+++ Hotel_Cashless_System_copy_20230915184816/Hotel_Cashless_System_copy_20230915184816.ino
@@ -1,120 +1,136 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+#include <SoftwareSerial.h>
+SoftwareSerial sim800(2, 4);
 
 #define TINY_GSM_MODEM_SIM800
 #include <TinyGsmClient.h>
 #include <ArduinoHttpClient.h>
-TinyGsm modem(Serial);
+TinyGsm modem(sim800);
 TinyGsmClient client(modem);
-HttpClient http = HttpClient(client, "missing url", 80); // Replace missing url with actual url
+HttpClient http = HttpClient(client, "bunihub.herokuapp.com", 80);
 
 #define SS_PIN 10
 #define RST_PIN 9
 #define BUZZER 6
-#define LED_GREEN 8
-#define LED_BLUE 7
-
+#define LED_RED 8
+#define LED_GREEN 7
 MFRC522 reader(SS_PIN, RST_PIN);
-String card = "";
+
+String tagID = "";
 
 void setup() {
-  // We are using the hardware serial 
   Serial.begin(9600);
 
+  SPI.begin();
+  reader.PCD_Init();
+
+  pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
   pinMode(BUZZER, OUTPUT);
 
-  Serial.println(F("HOTEL CASHLESS PAYMENT"));
+
+  digitalWrite(LED_GREEN, HIGH);
   delay(2000);
 
-  Serial.println(F("Connecting to Internet ..."));
-  if (modem.gprsConnect("", "", "")) {
-    Serial.print(" success");
-    delay(1000);
-  }
+
+  Serial.print("HOTEL CASHLESS SYSTEM");
 }
 
 void loop() {
-   if (!reader.PICC_IsNewCardPresent()) {
-      return false;
+  unsigned long savedtime1 = 0;
+  unsigned long savedtime2 = 0;
+  const long delay1 = 3000;
+  const long delay2 = 5000;
+
+  unsigned long currenttime = millis();
+  if (currenttime - savedtime1 >= delay1) {
+    savedtime1 = delay1;
+    int success_read = getID();
+    Serial.println("Waiting for scan");
+    if (success_read == 1) {
+      digitalWrite(BUZZER, HIGH);
+      delay(1000);
+      digitalWrite(BUZZER, LOW);
+      delay(1000);
     }
-    if (!reader.PICC_ReadCardSerial()) {
-      return false;
-    }
 
-    for (byte i = 0; i < reader.uid.size; i++) {
-    card += String(reader.uid.uidByte[i], DEC);
-  }
+    if (currenttime - savedtime2 >= delay2) {
+      savedtime2 = delay2;
+      Serial.println("Connecting to the Internet");
+      modem.gprsConnect("", "", "");
 
-    card = card.substring(0, 8);
-    Serial.println("Tag ID: " + card);
+      if (modem.isGprsConnected()) {
+        Serial.println("Making POST request...");
 
-    reader.PICC_HaltA();
-  
+        String contentType = "application/json";
+        String postData = GenerateJsonData();
+        Serial.println(postData);
+        int err = http.post("/bus", contentType, postData);
 
-    Serial.print(F("Setting buzzer High to confirm read"));
-    digitalWrite(BUZZER, HIGH);
-    delay(3000);
-    digitalWrite(BUZZER, LOW);
+        if (err != 0) {
+          Serial.println(F("failed to connect"));
+          tone(BUZZER, 10000, 500);
+          delay(1000);
+          noTone(BUZZER);
+          delay(1000);
+          return;
+        }
 
-    if (modem.isGprsConnected()) {
-      Serial.print(F("Setting Up blue green led to indicate connection"));
-      digitalWrite(LED_GREEN, HIGH);
+        int status = http.responseStatusCode();
+        Serial.print(F("Response status code: "));
+        Serial.println(status);
 
-      Serial.println("Making POST request...");
+        String response = http.responseBody();
+        Serial.println(F("Response:"));
+        Serial.println(response);
 
-      String contentType = "application/json";
-      String postData = GenerateJsonData();
-
-      int err = http.post("/", contentType, postData);
-
-      if (err != 0) {
-        Serial.println(F("failed to connect"));
-        delay(10000);
-        return;
+        if (status == 200) {
+          Serial.println(F("Setting Buzzer High"));
+          digitalWrite(LED_RED, HIGH);
+          delay(3000);
+          digitalWrite(LED_RED, LOW);
+          delay(3000);
+        }
+      } else {
+        tone(BUZZER, 10000, 500);
+        delay(1000);
+        noTone(BUZZER);
+        delay(1000);
       }
-
-      int status = http.responseStatusCode();
-      Serial.print(F("Response status code: "));
-      Serial.println(status);
-
-      String response = http.responseBody();
-      Serial.println(F("Response:"));
-      Serial.println(response);
-
-      if (status == 200) {
-        Serial.println(F("Successful"));
-        digitalWrite(LED_GREEN, HIGH);
-      }
-
-      http.stop();
-      Serial.println(F("Server disconnected"));
-    }
-    else {
-      Serial.println("Failed to connect to the network ");
-
-      digitalWrite(LED_BLUE, HIGH),
-      delay(1000);
-      digitalWrite(LED_BLUE, LOW);
-      delay(1000);
-      digitalWrite(LED_BLUE, HIGH),
-      delay(1000);
-      digitalWrite(LED_BLUE, LOW);
-      delay(1000);
     }
   }
-
-
-String GenerateJsonData() {
-  String type = "processPayment";
-  String aux = "{\n\t\"type\":\"";
-  aux.concat(String(type));
-  aux.concat("\",\n\t\"card_number\":\"");
-  aux.concat(String(card));
-  aux.concat("\n}");
-  return aux;
 }
 
 
+int getID() {
+  if (!reader.PICC_IsNewCardPresent()) {
+    return 0;
+  }
+
+  if (!reader.PICC_ReadCardSerial()) {
+    return 0;
+  }
+
+  for (byte i = 0; i < reader.uid.size; i++) {
+    tagID += String(reader.uid.uidByte[i], DEC);
+  }
+  tagID = tagID.substring(0, 8);
+  Serial.println("Tag ID: " + tagID);
+
+  Serial.println("");
+  reader.PICC_HaltA();
+  return 1;
+}
+
+String GenerateJsonData() {
+  String type = "processPayment";
+
+  String aux = "{\n\t\"type\":\"";
+  aux.concat(String(type));
+  aux.concat("\",\n\t\"card_number\":\"");
+  aux.concat(String(tagID));
+  aux.concat("\n}");
+  return aux;
+}
